eb972c416d4fbf862e781cb4032914af6b4efe56
linnefromice <linnefromice@users.noreply.github.com>
2020-09-21 08:29:23 +0000

    deploy: 46c1415803682d1704dd6a019cadf54261bcfe3d

---
+++ _next/static/mwNn-m5iABpSQeDEVk-AP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F2f1431f38e960874e6a7.css","static\u002Fchunks\u002Fpages\u002Findex-1ddc3d9ecf1902eb6a88.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
